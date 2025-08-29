6017b709150f8c486cb2df6c9fc0e6ea8ff78e18
Deepak Devadathan <deepak.devadathan@oracle.com>
2025-08-29 03:55:58 +0000

    clean up
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- func.go
+++ func.go
@@ -14,29 +14,27 @@ type Person struct {
 	Name string `json:"name"`
 }
 
-func main() {
+func InitializeRouter() *gin.Engine {
 	r := gin.Default()
 
-	// Gin route that uses same logic as myHandler
 	r.POST("/ping", func(c *gin.Context) {
 		p := &Person{Name: "World"}
 
-		// Decode request body into Person (if provided)
 		if err := c.ShouldBindJSON(p); err != nil {
-			// fallback to default "World" if no name provided
 			log.Printf("Error decoding body: %v", err)
 		}
 
-		// Create response
-		msg := struct {
-			Msg string `json:"message"`
-		}{
-			Msg: fmt.Sprintf("Hello %s", p.Name),
-		}
-
+		c.JSON(200, gin.H{
+			"message": fmt.Sprintf("Hello %s", p.Name),
+		})
 		log.Print("Inside Go Hello World function (Gin style)")
-		c.JSON(200, msg)
 	})
+
+	return r
+}
+
+func main() {
+	r := InitializeRouter()
 
 	if os.Getenv("FN_FORMAT") == "" {
 		// -- Local Run ---
